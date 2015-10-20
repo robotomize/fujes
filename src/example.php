ef4234f01eee2a4e34f3070eb2ff13977dc43136
Scrutinizer Auto-Fixer <auto-fixer@scrutinizer-ci.com>
2015-10-20 07:42:33 +0000

    Scrutinizer Auto-Fixes
    This commit consists of patches automatically generated for this project on https://scrutinizer-ci.com

--- src/example.php
+++ src/example.php
@@ -1,9 +1,9 @@
 <?php
 /**
- * This file is part of the fujes package.
- * @link    https://github.com/robotomize/fujes
- * @license http://www.opensource.org/licenses/mit-license.php MIT (see the LICENSE file)
- */
+     * This file is part of the fujes package.
+     * @link    https://github.com/robotomize/fujes
+     * @license http://www.opensource.org/licenses/mit-license.php MIT (see the LICENSE file)
+     */
 
 /**
  * Some examples of using the search.
